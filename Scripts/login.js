0496e7055b92f7a2616651ba4b2a20b60e9bca14
TicketSystems <131274739+TicketSystems@users.noreply.github.com>
2023-04-23 18:18:31 +0200

    login

---
+++ Scripts/login.js
@@ -0,0 +1,24 @@
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+      check()
+  }
+});
+
+    document.getElementById('submit').addEventListener('click', function() {
+        check()
+        });
+
+    function check() {
+        var code = document.getElementById('code').value;
+        if (code === 'base') {
+            window.location.href = 'navigationbase.html';
+        } else if (code === 'admin') {
+            window.location.href = 'navigationadmin.html';
+        } else {
+            document.getElementById('x').style.display = 'block';
+            setTimeout(function() {
+                document.getElementById('x').style.display = 'none';
+            }, 1000);
+    };
+}
+    document.getElementById("code").value = "";
