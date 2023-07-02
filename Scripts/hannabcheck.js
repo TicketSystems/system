ef05de9a8f3a58d3cb36990f4afa4b3fe5998a5c
TicketSystems <131274739+TicketSystems@users.noreply.github.com>
2023-07-02 11:32:41 +0200

    a

--- Scripts/hannabcheck.js
+++ Scripts/hannabcheck.js
@@ -1,5 +1,6 @@
 // Define the list of valid tickets
-const validTickets = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+//const validTickets = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+const validTickets = [];
 
 if (validTickets.length === 0) {
 // No valid tickets, hide 'cuccok' div and show 'error' div
