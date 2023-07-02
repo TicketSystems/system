5ec7675b1147f5087acedd6a781977d8e5bf7eeb
TicketSystems <131274739+TicketSystems@users.noreply.github.com>
2023-07-02 09:08:04 +0200

    a

---
+++ Scripts/hannabcheck.js
@@ -0,0 +1,61 @@
+// Define the list of valid tickets
+const validTickets = [12];
+
+if (validTickets.length === 0) {
+// No valid tickets, hide 'cuccok' div and show 'error' div
+document.querySelector('.cuccok').style.display = 'none';
+document.querySelector('.error').style.display = 'block';
+} else {
+// Valid tickets exist, show 'cuccok' div and hide 'error' div
+document.querySelector('.cuccok').style.display = 'block';
+document.querySelector('.error').style.display = 'none';
+}
+
+document.addEventListener("keyup", function(event) {
+  if (event.key === 'Enter') {
+    checkTicket()
+}
+});
+
+function playSound(audioName) {
+  let audio = new Audio(audioName)
+  audio.play()
+}
+
+function playArrive() {
+  let audio = new Audio("Köszöntelek a partin.m4a")
+  audio.play()
+}
+
+function checkTicket() {
+  // Get the value of the ticket number input field
+  const ticketNumber = parseInt(document.getElementById("ticketNum").value);
+  
+  // Check if the ticket number is in the list of valid tickets
+  if (validTickets.includes(ticketNumber)) {
+    // Remove the ticket number from the list of valid tickets
+    validTickets.splice(validTickets.indexOf(ticketNumber), 1);
+
+    // Display a success message
+    //alert(ticketNumber + "✅");
+    document.getElementById('pipa').style.display = 'block';
+    playSound("Elfogadva Kérlek csö.m4a")
+          setTimeout(function() {
+              document.getElementById('pipa').style.display = 'none';
+          }, 1000);
+
+    // Clear the input field
+    document.getElementById("ticketNum").value = "";
+  } else {
+    // Display an error message
+    //alert(ticketNumber + "❌");
+    document.getElementById('x').style.display = 'block';
+    playSound("Megtagadva Próbáld m.m4a")
+          setTimeout(function() {
+              document.getElementById('x').style.display = 'none';
+          }, 1000);
+
+    // Clear the input field
+    document.getElementById("ticketNum").value = "";
+  }
+}
